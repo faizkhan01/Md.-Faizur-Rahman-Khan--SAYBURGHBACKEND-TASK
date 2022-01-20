80148872fab22de99cd35ffa0d06f0c057534330
Md. Faizur Rahman Khan <khan.nayeem8662@gmail.com>
2022-01-20 10:33:23 +0600

    user authorization, create post and delete post added

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require("apollo-server");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const {MONGODB} = require('./config');
-
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config");
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req }),
 });
 
-mongoose.connect(MONGODB, {useNewUrlParser: true})
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-      console.log('Database connected successfully');
-      return server.listen({port: 5000})
+    console.log("Database connected successfully");
+    return server.listen({ port: 5000 });
   })
   .then((res) => {
-      console.log(`Server running at ${res.url}`)
+    console.log(`Server running at ${res.url}`);
   })
-  .catch(err => {
-    console.error(err)
-  })
+  .catch((err) => {
+    console.error(err);
+  });
